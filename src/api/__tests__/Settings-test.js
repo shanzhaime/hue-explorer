c2ec8d0b09ef87bc22f0e494f58a5dc20fecb835
Cat Chen <cathsfz@gmail.com>
2018-05-15 21:39:37 -0700

    Added tests for Settings

---
+++ src/api/__tests__/Settings-test.js
@@ -0,0 +1,52 @@
+import Settings from '../Settings';
+import Storage from '../Storage';
+
+jest.mock('../Storage');
+
+beforeEach(() => {
+  let storedId = null;
+  Storage.mock.instances[0].write = jest.fn().mockImplementation((id) => {
+    storedId = id;
+  });
+  Storage.mock.instances[0].read = jest.fn().mockImplementation(() => {
+    return storedId;
+  });
+});
+
+it('reads settings previously written', () => {
+  expect(Storage.mock.instances).toHaveLength(1);
+  const storageInstance = Storage.mock.instances[0];
+
+  const firstSettings = {
+    appId: Math.random().toString(),
+    clientId: Math.random().toString(),
+    clientSecret: Math.random().toString(),
+  };
+
+  Settings.write(firstSettings);
+  expect(storageInstance.write).toHaveBeenCalledTimes(1);
+  expect(Settings.read()).toEqual(firstSettings);
+  expect(storageInstance.read).toHaveBeenCalledTimes(1);
+
+  const secondSettings = {
+    appId: Math.random().toString(),
+    clientId: Math.random().toString(),
+    clientSecret: Math.random().toString(),
+  };
+
+  Settings.write(secondSettings);
+  expect(storageInstance.write).toHaveBeenCalledTimes(2);
+  expect(Settings.read()).toEqual(secondSettings);
+  expect(storageInstance.read).toHaveBeenCalledTimes(2);
+
+  const thirdSettings = {
+    appId: Math.random().toString(),
+    clientId: Math.random().toString(),
+    clientSecret: Math.random().toString(),
+  };
+
+  Settings.write(thirdSettings);
+  expect(storageInstance.write).toHaveBeenCalledTimes(3);
+  expect(Settings.read()).toEqual(thirdSettings);
+  expect(storageInstance.read).toHaveBeenCalledTimes(3);
+});
