e30be447b681e2f36032037087cfbaaa97dab2b6
Cat Chen <cathsfz@gmail.com>
2018-06-23 14:09:12 -0700

    Created test for JsonObject

---
+++ src/ui/json/__tests__/JsonObject-test.js
@@ -0,0 +1,47 @@
+import JsonObject from '../JsonObject';
+import React from 'react';
+import { mount } from 'enzyme';
+
+it('should render empty value', () => {
+  const wrapper = mount(<JsonObject json={{}} />);
+  expect(wrapper.text()).toBe('{}');
+});
+
+it('should render value in mixed types', () => {
+  const wrapper = mount(
+    <JsonObject
+      json={{
+        a: null,
+        b: true,
+        c: false,
+        d: 42,
+        e: NaN,
+        f: 'Let\'s say "yes".',
+        g: [3.14, 2.22],
+        h: { x: { y: { z: 42 } } },
+      }}
+    />,
+  );
+  // prettier-ignore
+  expect(wrapper.text()).toBe([
+    '{',
+    '  "a": null,',
+    '  "b": true,',
+    '  "c": false,',
+    '  "d": 42,',
+    '  "e": NaN,',
+    '  "f": "Let\'s say \\"yes\\".",',
+    '  "g": [',
+    '    3.14,',
+    '    2.22',
+    '  ],',
+    '  "h": {',
+    '    "x": {',
+    '      "y": {',
+    '        "z": 42',
+    '      }',
+    '    }',
+    '  }',
+    '}'
+  ].join(''));
+});
