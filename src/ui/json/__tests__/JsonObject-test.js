18234c73307101365ce8cdd206385da63a13854a
Cat Chen <catchen@catchen.me>
2018-06-23 22:01:08 -0700

    Made JSON editor 100% coverage

--- src/ui/json/__tests__/JsonObject-test.js
+++ src/ui/json/__tests__/JsonObject-test.js
@@ -45,3 +45,9 @@ it('should render value in mixed types', () => {
     '}'
   ].join(''));
 });
+
+it('should not render other types', () => {
+  expect(() => {
+    mount(<JsonObject json={{ value: function() {} }} />);
+  }).toThrow();
+});
