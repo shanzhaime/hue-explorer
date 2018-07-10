67a79074ff37a73f575ba7e9c42e18b52a16dde9
Cat Chen <catchen@catchen.me>
2018-07-09 23:42:15 -0700

    Created raw Schedule card view

--- src/ui/SchedulesView.js
+++ src/ui/SchedulesView.js
@@ -1,4 +1,6 @@
-import JsonEditor from './json/JsonEditor';
+import CenterCard from './CenterCard';
+import LoadingIndicator from './LoadingIndicator';
+import Schedule from './Schedule';
 import HueBridge from '../api/HueBridge';
 import HueBridgeList from '../api/HueBridgeList';
 import React, { Component } from 'react';
@@ -9,6 +11,7 @@ class SchedulesView extends Component {
     this.state = {
       bridge: null,
       json: null,
+      loading: false,
     };
   }
 
@@ -35,21 +38,51 @@ class SchedulesView extends Component {
     const bridge = this.getActiveBridge();
     this.setState({
       bridge,
+      loading: true,
     });
     bridge.fetch('/schedules').then((json) => {
       console.log(json);
       this.setState({
         json,
+        loading: false,
       });
     });
   }
 
   render() {
-    return (
-      <div>
-        <JsonEditor json={this.state.json} />
-      </div>
-    );
+    if (this.state.loading) {
+      return (
+        <CenterCard>
+          <h5 className="card-header">Loading...</h5>
+          <div className="card-body">
+            <LoadingIndicator />
+          </div>
+        </CenterCard>
+      );
+    } else if (
+      this.state.json === null ||
+      Object.keys(this.state.json).length === 0
+    ) {
+      return (
+        <div className="alert alert-info my-3" role="alert">
+          No schedules.
+        </div>
+      );
+    } else {
+      return (
+        <div className="card-columns my-3">
+          {Object.keys(this.state.json).map((key) => {
+            return (
+              <Schedule
+                json={this.state.json[key]}
+                scheduleId={key}
+                key={key}
+              />
+            );
+          })}
+        </div>
+      );
+    }
   }
 }
 
